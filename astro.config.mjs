27ac0f4b3c99d0ac116a2122c0d8306a84272af7
Potato22 <potatooosss22@gmail.com>
2025-10-14 19:11:50 +0700

    goddamit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://commission.pottoart.uk',
+    site: 'https://pottoart.uk',
 
     //base: 'commission' //repo name
     //does fucking nothing.
